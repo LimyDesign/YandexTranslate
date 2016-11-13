92572c92ba08293648cf5cb45d2eb8cecb42e411
Arsen Bespalov <dev@spadix.ru>
2016-11-13 19:56:16 +0800

    Добавлен класс иконок.

---
+++ src/ru/limydesign/plugins/yandex/translate/YandexTranslateIcons.java
@@ -0,0 +1,12 @@
+package ru.limydesign.plugins.yandex.translate;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author Arsen Bespalov
+ */
+public interface YandexTranslateIcons {
+    Icon YANDEX_TRANSLATE_ICON = IconLoader.getIcon("/icons/yandex_translate.png");
+}
